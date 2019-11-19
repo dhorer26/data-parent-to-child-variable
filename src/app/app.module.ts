2757eba8b9b9b216cb228c3efe225459e6270086
Ravindra Dhore <ravindrad@ansh.com>
2019-11-19 16:53:37 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayStudentComponent } from './students/display-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayStudentComponent,
   ],
   imports: [
     BrowserModule,
